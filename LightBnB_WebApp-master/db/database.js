4472e7085ecc5b6de6af75218c8efad36073d32c
Monica Wolfe <mcwolfe2011@gmail.com>
2023-04-06 18:30:18 -0700

    Refactor getAllReservations function

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -79,7 +79,26 @@ const addUser = function(user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+      SELECT properties.*, reservations.*, avg(rating) as average_rating
+      FROM reservations
+      JOIN properties ON reservations.property_id = properties.id
+      JOIN property_reviews ON property_reviews.property_id = properties.id
+      JOIN users ON reservations.guest_id = users.id
+      WHERE reservations.guest_id = $1 
+      AND reservations.end_date < now()::date
+      GROUP BY properties.id, reservations.id
+      ORDER BY start_date
+      LIMIT $2;
+      `;
+
+  return pool.query(queryString, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      return err.message;
+    });
 };
 
 /// Properties
